681dcf4f48d9077627fb4576c383fba244df3557
huyanyou <1026855639@qq.com>
2022-09-25 13:23:28 +0800

    henu-class-init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+// import { ref } from 'vue'
+let title = $ref('Hello')
+
+onMounted(()=>{
+  title = 'Hello World'
+})
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
